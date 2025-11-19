860f358b273f21f4a3a499b99a076ade340eaa71
Jonas petersen <jonas.petersen@forgis.com>
2025-11-19 12:57:43 +0100

    Add favicon links to root.tsx for correct favicon usage

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -11,6 +11,11 @@ import globalStylesUrl from "./styles/global.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: globalStylesUrl },
+  { rel: "icon", type: "image/x-icon", href: "/favicon_io/favicon.ico" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon_io/favicon-16x16.png" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon_io/favicon-32x32.png" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon_io/apple-touch-icon.png" },
+  { rel: "manifest", href: "/favicon_io/site.webmanifest" },
 ];
 
 export const meta: MetaFunction = () => ([
